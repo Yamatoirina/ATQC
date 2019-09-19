0481357c190e0e2bfc4a11e754020dc30186f8a0
Yamatoirina <yamatoirina@gmail.com>
2019-09-19 23:11:43 +0300

    Hometask 03

---
+++ src/main/java/hometask_lesson_03/Education.java
@@ -0,0 +1,14 @@
+package hometask_lesson_03;
+
+
+public class Education {
+    public static double a = 5;
+    public static double b = 10;
+    public static void main(String[] args) {
+
+        double result = (a /b);
+
+        System.out.println(result);
+    }
+}
+
